431b86e91b67e96068eaa650e81b0163d3026163
Riccardo Setti <giskard@autistici.org>
2019-07-29 16:17:59 +0200

    167394049: make az-export.sh  more robust (#15)

--- utils/az-export.sh
+++ utils/az-export.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # Azure export script
-# 
+#
 # It exports environment variables used by other tools
 # used to provision and maintain the Azure infrastructure.
-# 
+#
 # teamdigitale.governo.it
+
+set -e
+set -u
 
 if [ ! -f .env ]; then
   echo """
@@ -29,5 +32,5 @@ export TF_VAR_default_admin_username=${DEFAULT_ADMIN_USER}
 export TERRAFORM_STORAGE_ACCOUNT_NAME=${TERRAFORM_STORAGE_ACCOUNT_NAME}
 export TERRAFORM_CONTAINER_NAME=${TERRAFORM_CONTAINER_NAME}
 export ARM_ACCESS_KEY=$(az keyvault secret show --name ${TERRAFORM_VAULT_KEY_STORAGE_ACCOUNT} --vault-name ${VAULT_NAME} --query value -o tsv)
-export PACKER_SP_ID=$(az ad sp list --display-name ${PACKER_SP_NAME} --query '[0].{"id":"appId"}' -o tsv) 
+export PACKER_SP_ID=$(az ad sp list --display-name ${PACKER_SP_NAME} --query '[0].{"id":"appId"}' -o tsv)
 export PACKER_SP_SECRET=$(az keyvault secret show --name ${PACKER_VAULT_KEY} --vault-name ${VAULT_NAME} --query value -o tsv)
