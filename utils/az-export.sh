7d337816255f2a6183089fe676ca5e2c15725431
Roberto Chessa <r.chessa@beetobit.com>
2019-08-06 16:57:01 +0200

    rollback PACKER values from az-export

--- utils/az-export.sh
+++ utils/az-export.sh
@@ -29,3 +29,5 @@ export TF_VAR_default_admin_username=${DEFAULT_ADMIN_USER}
 export TERRAFORM_STORAGE_ACCOUNT_NAME=${TERRAFORM_STORAGE_ACCOUNT_NAME}
 export TERRAFORM_CONTAINER_NAME=${TERRAFORM_CONTAINER_NAME}
 export ARM_ACCESS_KEY=$(az keyvault secret show --name ${TERRAFORM_VAULT_KEY_STORAGE_ACCOUNT} --vault-name ${VAULT_NAME} --query value -o tsv)
+export PACKER_SP_ID=$(az ad sp list --display-name ${PACKER_SP_NAME} --query '[0].{"id":"appId"}' -o tsv)
+export PACKER_SP_SECRET=$(az keyvault secret show --name ${PACKER_VAULT_KEY} --vault-name ${VAULT_NAME} --query value -o tsv)
