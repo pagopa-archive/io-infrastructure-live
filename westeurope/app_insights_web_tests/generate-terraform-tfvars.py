c5c914889f447b415e339d27b597a5fc15c6ca34
Riccardo Setti <giskard@autistici.org>
2019-10-15 09:44:00 +0200

    [#169145231] split out xml generation (#69)

--- westeurope/app_insights_web_tests/generate-terraform-tfvars.py
+++ westeurope/app_insights_web_tests/generate-terraform-tfvars.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+from base64 import b64encode
 import shlex
 import json
 import subprocess
 
+from jinja2 import Template
 
 TERRAFORM_TFVARS="""
 # The module creates and configure application insights web tests
@@ -36,14 +38,35 @@ web_tests = [ @@WEB_TESTS@@ ]
 WEB_TEST_HCL="""
     {
         "name"        = "@@web_test_name@@",
-        "method"      = "@@web_test_method@@",
-        "url"         = "@@web_test_url@@",
-        "body"        = "@@web_test_body@@"
-        "headers_xml" = <<XML
-                        "@@web_test_headers_xml@@"
+        "xml"         = <<XML
+"@@web_test_xml@@"
 XML
     },
 """
+
+WEB_TEST_XML= Template("""
+  <WebTest Name="{{test["name"] }}" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True"
+	   CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds=""
+	   xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010"
+	   Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default"
+	   StopOnError="False" RecordedResultFile="" ResultsLocale="">
+    <Items>
+      <Request Method="{{ test["method"] }}" Guid="a5f10126-e4cd-570d-961c-cea43999a200"
+        Version="1.1" Url="{{ test["url"] }}"
+	ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True"
+	Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200"
+	ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False">
+	<Headers>
+          {{ test_headers }}
+	</Headers>
+        {% if test["method"] == "POST" %}
+	<StringHttpBody ContentType="application/json">{{ test["s_body"] }}</StringHttpBody>
+        {% endif %}
+      </Request>
+    </Items>
+  </WebTest>
+"""
+)
 
 def get_secret_value(header):
     cmd = shlex.split("az keyvault secret show --name app-insight-web-tests-%s --vault-name io-dev-keyvault" % header)
@@ -63,10 +86,10 @@ final = []
 for test in web_tests:
     web_test = WEB_TEST_HCL
     web_test = web_test.replace("@@web_test_name@@", test["name"])
-    web_test = web_test.replace("@@web_test_url@@", test["url"])
 
-    web_test = web_test.replace("@@web_test_method@@", test.get("method", "GET"))
-    web_test = web_test.replace("@@web_test_body@@", str(test.get("body", "")))
+    web_test_xml = WEB_TEST_XML
+    b_body = b64encode(bytearray(str(test.get("body", "")), "utf-8"))
+    test["s_body"] = b_body.decode("utf-8")
 
     try:
         if test["headers_xml"]:
@@ -79,6 +102,9 @@ for test in web_tests:
 
     except KeyError:
         web_test = web_test.replace("@@web_test_headers_xml@@", "")
+
+    r_web_test_xml = web_test_xml.render(test=test)
+    web_test = web_test.replace("@@web_test_xml@@", r_web_test_xml)
 
     final.append(web_test)
 
