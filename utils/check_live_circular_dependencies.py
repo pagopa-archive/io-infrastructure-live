8818562633c1c6408262e95d758e91c0c662fab6
Riccardo Setti <rsetti@gmail.com>
2019-11-11 12:20:10 +0100

    Add as_text and as_image function

--- utils/check_live_circular_dependencies.py
+++ utils/check_live_circular_dependencies.py
@@ -4,13 +4,13 @@ import argparse
 import os
 import sys
 
-from graphviz import Digraph
 import hcl
-
-dot = Digraph(comment="tfvars dev")
 
 parser = argparse.ArgumentParser(
     description="Scan live environment repository for circular dependencies"
+)
+parser.add_argument(
+    "action", nargs="?", choices=("as_text", "as_image"), default="as_text"
 )
 parser.add_argument("-d", "--directory", help="relative path")
 parser.add_argument("-o", "--output", help="output file")
@@ -18,22 +18,62 @@ parser.add_argument("-o", "--output", help="output file")
 args = parser.parse_args()
 
 
-for root, dirs, files in os.walk(args.directory):
-    if "terragrunt-cache" in root:
-        continue
-    for name in files:
-        if name == "terraform.tfvars":
-            tf_filename = os.path.join(root, name)
-            with open(tf_filename, "r") as fp:
-                obj = hcl.load(fp)
-                try:
-                    dot.node(root, tf_filename)
-                    dependencies = obj["terragrunt"]["dependencies"]["paths"]
-                    for dep in dependencies:
-                        root_dep = os.path.join(args.directory, dep[3:])
-                        dot.edge(root, root_dep )
-                except KeyError:
-                    pass
+def as_text():
+    dephell = {}
+    for root, dirs, files in os.walk(args.directory):
+        if "terragrunt-cache" in root:
+            continue
+        for name in files:
+            if name == "terraform.tfvars":
+                tf_filename = os.path.join(root, name)
+                with open(tf_filename, "r") as fp:
+                    obj = hcl.load(fp)
+                    try:
+                        dephell[root] = []
+                        dependencies = obj["terragrunt"]["dependencies"]["paths"]
+                        for dep in dependencies:
+                            dephell[root].append(os.path.join(args.directory, dep[3:]))
+                    except KeyError:
+                        pass
+
+    success = False
+    try:
+        for k in dephell.keys():
+            for d in dephell[k]:
+                if k in dephell[d]:
+                    print("Circular dep found between %s and %s" % (k, d))
+                    success = True
+
+    except KeyError as e:
+        print("%s :: has possible dependencies problem" % e)
+    return success
 
 
-dot.render(args.output, view=True)
+def as_image():
+    from graphviz import Digraph
+
+    dot = Digraph(comment="tfvars dev")
+    for root, dirs, files in os.walk(args.directory):
+        if "terragrunt-cache" in root:
+            continue
+        for name in files:
+            if name == "terraform.tfvars":
+                tf_filename = os.path.join(root, name)
+                with open(tf_filename, "r") as fp:
+                    obj = hcl.load(fp)
+                    try:
+                        dot.node(root, root)
+                        dependencies = obj["terragrunt"]["dependencies"]["paths"]
+                        for dep in dependencies:
+                            root_dep = os.path.join(args.directory, dep[3:])
+                            dot.edge(root, root_dep)
+                    except KeyError:
+                        pass
+
+    dot.render(args.output, view=True)
+
+
+if args.action == "as_text":
+    sys.exit(as_text())
+else:
+    as_image()
