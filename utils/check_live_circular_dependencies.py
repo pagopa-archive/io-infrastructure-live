fb072b7814a58255288c01aa5b10317c36cc896f
Riccardo Setti <rsetti@gmail.com>
2019-11-11 11:07:59 +0100

    Styles checks and exclude terragrunt-cache

--- utils/check_live_circular_dependencies.py
+++ utils/check_live_circular_dependencies.py
@@ -7,22 +7,26 @@ import sys
 from graphviz import Digraph
 import hcl
 
-dot = Digraph(comment='tfvars dev')
+dot = Digraph(comment="tfvars dev")
 
-parser = argparse.ArgumentParser(description='Scan live environment repository for circular dependencies')
-parser.add_argument('-d', '--directory', help='relative path')
-parser.add_argument('-o', '--output', help='output file')
+parser = argparse.ArgumentParser(
+    description="Scan live environment repository for circular dependencies"
+)
+parser.add_argument("-d", "--directory", help="relative path")
+parser.add_argument("-o", "--output", help="output file")
 
 args = parser.parse_args()
 
 
 for root, dirs, files in os.walk(args.directory):
+    if "terragrunt-cache" in root:
+        continue
     for name in files:
         if name == "terraform.tfvars":
-            with open(os.path.join(root, name), 'r') as fp:
+            with open(os.path.join(root, name), "r") as fp:
                 obj = hcl.load(fp)
                 try:
-                    dot.node(os.path.join(root,name), os.path.join(root,name))
+                    dot.node(os.path.join(root, name), os.path.join(root, name))
                     dependencies = obj["terragrunt"]["dependencies"]["paths"]
                     for dep in dependencies:
                         dot.edge(os.path.join(root, name), dep)
