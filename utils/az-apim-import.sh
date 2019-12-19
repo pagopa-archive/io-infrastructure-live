b0826090ee14d50e34438b627761709713622f71
Luca Prete <preteluca@gmail.com>
2019-12-19 15:38:46 +0100

    Add threads

--- utils/az-apim-import.sh
+++ utils/az-apim-import.sh
@@ -20,7 +20,7 @@ src_apim_product_id=starter
 src_deployment_name=service_agid_apim_prod_name
 
 # How many resources to import at each run (ARM limit 800)
-resource_count_pagination=700
+resource_count_pagination=1
 
 function usage {
   echo "Usage: az-apim-import [--help] [-d|--dry-run]"
@@ -45,12 +45,54 @@ function split {
   echo "${array[@]}"
 }
 
+# This code is executed in a separate thread
+# 
+# Input values:
+# $1 - Destination resource group
+# $2 - Destination APIM name
+# $3 - Resource name
+# $4 - Resources to import
+# $5 - Limit down (used for file and deployment name)
+# $6 - Limit up (used for file and deployment name)
+function generate_arm_template {
+  dst_resource_group=$1
+  dst_apim_name=$2
+  resource_name=$3
+  resources=$4
+  limit_down=$5
+  limit_up=$6
+
+  {
+    echo "{"
+    echo "  \"\$schema\": \"https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#\","
+    echo "  \"contentVersion\": \"1.0.0.0\","
+    echo "  \"parameters\": {"
+    echo "    \"$dst_deployment_name\": {"
+    echo "      \"defaultValue\": \"$dst_apim_name\","
+    echo "      \"type\": \"String\""
+    echo "    }"
+    echo "  },"
+    echo "  \"variables\": {},"
+    echo "  \"resources\": $resources"
+    echo "}"
+  } > apim-$resource_name-$limit_down-$limit_up.json
+
+  if [[ $dry_run = false ]]; then
+    echo "[$resource_name] Deploying ARM template $resource_name from $limit_down to $limit_up"
+    az group deployment create --name $resource_name-sync-job-$now-$limit_down-$limit_up --resource-group $dst_resource_group --template-file apim-$resource_name-$limit_down-$limit_up.json > /dev/null
+  fi
+
+  # Cleanup
+  az group deployment delete -g $dst_resource_group --name $resource_name-sync-job-$now-$limit_down-$limit_up
+  rm -rf "apim-$resource_name-$limit_down-$limit_up.json"
+}
+
 # Generates and uploads ARM templates to the destination APIM
 # 
 # Input values:
 # $1 - resource_type: the type of resource to upload
 # $2 - no_name:       the resource names to exclude form the import
-function generate_arm_template {
+function generate_arm_template_threaded {
   local limit_down=0
   local resource_type=$1
   local no_names=$2
@@ -79,31 +121,18 @@ function generate_arm_template {
   # based on the pagination parameter set. Save each of them
   # in a file called apim-{RESOURCE-TYPE}-{LOWER-LIMIT}-{UPPER-LIMIT}.json.
   # Then, deploy them one by one, if dry_run is 0.
-  while [[ $limit_down -lt $resource_count ]] 
+  while [[ $limit_down -lt $resource_count ]]
   do
+    # If more then 10 threads are running, wait for 5 seconds, before open new ones
+    while [[ $(ps aux | grep -i "az group deployment create" | wc -l) -gt 20 ]];
+    do
+      sleep 1
+    done
+
     echo "[$resource_name] Processing resources from $limit_down to $limit_up"
     resources=$(cat "$src_template_file" | jq -r --arg LIMIT_DOWN "$limit_down" --arg LIMIT_UP "$limit_up" --arg RESOURCE_TYPE "$resource_type" --arg NO_NAMES "$no_names" '[.resources[] | select((.type==$RESOURCE_TYPE) and (.name | test($NO_NAMES) | not)) | del(.dependsOn)] | .[$LIMIT_DOWN|tonumber:$LIMIT_UP|tonumber]')
 
-    {
-      echo "{"
-      echo "  \"\$schema\": \"https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#\","
-      echo "  \"contentVersion\": \"1.0.0.0\","
-      echo "  \"parameters\": {"
-      echo "    \"$dst_deployment_name\": {"
-      echo "      \"defaultValue\": \"$dst_apim_name\","
-      echo "      \"type\": \"String\""
-      echo "    }"
-      echo "  },"
-      echo "  \"variables\": {},"
-      echo "  \"resources\": $resources"
-      echo "}"
-    } > apim-$resource_name-$limit_down-$limit_up.json
-
-    if [[ $dry_run = false ]]; then
-      echo "[$resource_name] Deploying ARM template $resource_name from $limit_down to $limit_up"
-      az group deployment create --name $resource_name-sync-job-$now-$limit_down-$limit_up --resource-group $dst_resource_group --template-file apim-$resource_name-$limit_down-$limit_up.json > /dev/null
-      az group deployment wait --name $resource_name-sync-job-$now-$limit_down-$limit_up --resource-group $dst_resource_group --updated > /dev/null
-    fi
+    generate_arm_template "$dst_resource_group" "$dst_apim_name" "$resource_name" "$resources" "$limit_down" "$limit_up" &
 
     let limit_down=limit_up+1
     let limit_check=$resource_count-$limit_up
@@ -144,9 +173,9 @@ awk -v src_apim_product_id="${src_apim_product_id}" -v dst_apim_product_id="${ds
 # Cleanup old APIM product name and set the new one
 awk -v src_deployment_name="${src_deployment_name}" -v dst_deployment_name="${dst_deployment_name}" '{gsub(src_deployment_name,dst_deployment_name)}1' "$src_template_file" > "$src_template_file".tmp && mv "$src_template_file".tmp "$src_template_file"
 
-generate_arm_template "Microsoft.ApiManagement/service/users" "\/1"
-generate_arm_template "Microsoft.ApiManagement/service/groups" "\/administrators|\/developers|\/guests"
-generate_arm_template "Microsoft.ApiManagement/service/groups/users" "\/administrators|\/developers|\/guests"
-generate_arm_template "Microsoft.ApiManagement/service/subscriptions" "\/master"
+generate_arm_template_threaded "Microsoft.ApiManagement/service/users" "\/1"
+generate_arm_template_threaded "Microsoft.ApiManagement/service/groups" "\/administrators|\/developers|\/guests"
+generate_arm_template_threaded "Microsoft.ApiManagement/service/groups/users" "\/administrators|\/developers|\/guests"
+generate_arm_template_threaded "Microsoft.ApiManagement/service/subscriptions" "\/master"
 
 echo -e "\n"
